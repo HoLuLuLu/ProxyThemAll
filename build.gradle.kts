51bb26140cf4a472c3a29b8ea50a3c80f0bce80e
HoLuLuLu <17492312+HoLuLuLu@users.noreply.github.com>
2026-01-07 10:50:48 +0100

    Add JUnit 4 runtime dependency required by IntelliJ Platform test framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation(libs.mockk)
     testImplementation(libs.opentest4j)
+    // JUnit 4 runtime dependency required by IntelliJ Platform test framework
+    testRuntimeOnly("junit:junit:4.13.2")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
